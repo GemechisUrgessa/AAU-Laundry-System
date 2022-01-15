1a56f9d3dfde529e6a67794b21b27b67ef93b4ab
GemechisUrgessa <gemechisurgessa@gmail.com>
2022-01-15 21:00:26 +0300

    maven project initialized

---
+++ aau_laundry_system/src/main/java/com/web_project/aau_laundry_system/AauLaundrySystemApplication.java
@@ -0,0 +1,13 @@
+package com.web_project.aau_laundry_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AauLaundrySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AauLaundrySystemApplication.class, args);
+	}
+
+}
---
+++ aau_laundry_system/src/test/java/com/web_project/aau_laundry_system/AauLaundrySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_project.aau_laundry_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AauLaundrySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
