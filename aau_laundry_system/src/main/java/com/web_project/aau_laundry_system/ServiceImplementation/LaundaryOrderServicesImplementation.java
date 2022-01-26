24cec2d256eea3364e3160f6ef1b6969d2ecf349
zeamanual <91523382+zeamanual@users.noreply.github.com>
2022-01-26 18:26:38 +0300

    Add files via upload
    Services implementation added

---
+++ aau_laundry_system/src/main/java/com/web_project/aau_laundry_system/ServiceImplementation/LaundaryOrderServicesImplementation.java
@@ -0,0 +1,73 @@
+package com.aaulaundary.aau_laundary_system.ServiceImplementation;
+
+import java.util.List;
+
+import com.aaulaundary.aau_laundary_system.Repositories.LaundaryOrderRepository;
+import com.aaulaundary.aau_laundary_system.Services.LaundaryOrderServices;
+import com.aaulaundary.aau_laundary_system.models.LaundaryOrder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LaundaryOrderServicesImplementation implements LaundaryOrderServices {
+    @Autowired
+    private LaundaryOrderRepository laundaryOrderRepository;
+
+    @Override
+    public LaundaryOrder saveLaundarOrder(LaundaryOrder laundaryOrder) {
+        return laundaryOrderRepository.save(laundaryOrder);
+    }
+
+    @Override
+    public LaundaryOrder findOrder(Long id) {
+        if (laundaryOrderRepository.existsById(id)){
+            return laundaryOrderRepository.findById(id).get();
+       }
+       return null;
+    }
+
+    @Override
+    public void updateOrder(Long id, LaundaryOrder laundaryOrder) {
+        if (laundaryOrderRepository.existsById(id)){
+            laundaryOrderRepository.save(laundaryOrder);
+        }  
+        
+    }
+
+    @Override
+    public List<LaundaryOrder> findAllOrder() {
+        return laundaryOrderRepository.findAll();
+    
+}
+
+    @Override
+    public List<LaundaryOrder> findOrderByUsername(String username) {
+      
+        return laundaryOrderRepository.findOrderByUserName(username);
+    }
+
+    @Override
+    public List<LaundaryOrder> findbByRecentOrders (){
+            return laundaryOrderRepository.findbByRecentOrders();
+    }
+
+    @Override
+    public int findOrderCount() {
+         return laundaryOrderRepository.findOrderCount();
+    
+    }
+
+    @Override
+    public float findTotalEarning() {
+        if(laundaryOrderRepository.findAll().size()>0){
+            return laundaryOrderRepository.findTotalEarning();
+        }
+        else{
+            return 0;
+        }
+        
+    }
+
+}
+
