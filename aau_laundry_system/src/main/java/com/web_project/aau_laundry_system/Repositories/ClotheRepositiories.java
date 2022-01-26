c9e3cf842667fdd2a02ab920a80717a5f061153c
Max <91595853+GemechisUrgessa@users.noreply.github.com>
2022-01-26 18:49:30 +0300

    Add files via upload
    Repos added

---
+++ aau_laundry_system/src/main/java/com/web_project/aau_laundry_system/Repositories/ClotheRepositiories.java
@@ -0,0 +1,9 @@
+package com.aaulaundary.aau_laundary_system.Repositories;
+
+import com.aaulaundary.aau_laundary_system.models.Clothe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClotheRepositiories extends JpaRepository<Clothe,String> {
+     
+}
