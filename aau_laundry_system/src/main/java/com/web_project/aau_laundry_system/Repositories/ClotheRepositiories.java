940689b22438455a7faeeb9597c83e5e9510135a
Max <91595853+GemechisUrgessa@users.noreply.github.com>
2022-01-26 18:51:46 +0300

    Delete ClotheRepositiories.java

--- aau_laundry_system/src/main/java/com/web_project/aau_laundry_system/Repositories/ClotheRepositiories.java
+++
@@ -1,9 +0,0 @@
-package com.aaulaundary.aau_laundary_system.Repositories;
-
-import com.aaulaundary.aau_laundary_system.models.Clothe;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClotheRepositiories extends JpaRepository<Clothe,String> {
-     
-}
